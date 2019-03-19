e8b6e9f88451029b92bd66088095bb3488d02f00
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-03-19 22:21:57 +0100

    WIP - backport l10n_fr_certification + disable useless B2B feature

--- l10n_fr_certification/models/res_company.py
+++ l10n_fr_certification/models/res_company.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, api
+from openerp import fields, models, api
 
 UNALTERABLE_COUNTRIES = ['FR', 'MF', 'MQ', 'NC', 'PF', 'RE', 'GF', 'GP', 'TF']
 
@@ -11,29 +12,32 @@ class ResCompany(models.Model):
 
     # To do in master : refactor to set sequences more generic
 
-    l10n_fr_secure_sequence_id = fields.Many2one('ir.sequence', 'Sequence to use to ensure the securisation of data', readonly=True)
+    # <GRAP> Disable useless feature
+    # l10n_fr_secure_sequence_id = fields.Many2one('ir.sequence', 'Sequence to use to ensure the securisation of data', readonly=True)
 
-    @api.model
-    def create(self, vals):
-        company = super(ResCompany, self).create(vals)
-        #when creating a new french company, create the securisation sequence as well
-        if company._is_accounting_unalterable():
-            sequence_fields = ['l10n_fr_secure_sequence_id']
-            company._create_secure_sequence(sequence_fields)
-        return company
+    # @api.model
+    # def create(self, vals):
+    #     company = super(ResCompany, self).create(vals)
+    #     #when creating a new french company, create the securisation sequence as well
+    #     if company._is_accounting_unalterable():
+    #         sequence_fields = ['l10n_fr_secure_sequence_id']
+    #         company._create_secure_sequence(sequence_fields)
+    #     return company
 
-    @api.multi
-    def write(self, vals):
-        res = super(ResCompany, self).write(vals)
-        #if country changed to fr, create the securisation sequence
-        for company in self:
-            if company._is_accounting_unalterable():
-                sequence_fields = ['l10n_fr_secure_sequence_id']
-                company._create_secure_sequence(sequence_fields)
-        # fiscalyear_lock_date can't be set to a prior date
-        if 'fiscalyear_lock_date' in vals or 'period_lock_date' in vals:
-            self._check_lock_dates(vals)
-        return res
+    # @api.multi
+    # def write(self, vals):
+    #     res = super(ResCompany, self).write(vals)
+    #     #if country changed to fr, create the securisation sequence
+    #     for company in self:
+    #         if company._is_accounting_unalterable():
+    #             sequence_fields = ['l10n_fr_secure_sequence_id']
+    #             company._create_secure_sequence(sequence_fields)
+    #     # fiscalyear_lock_date can't be set to a prior date
+    #     if 'fiscalyear_lock_date' in vals or 'period_lock_date' in vals:
+    #         self._check_lock_dates(vals)
+    #     return res
+    # </GRAP> Disable useless feature
+
 
     def _create_secure_sequence(self, sequence_fields):
         """This function creates a no_gap sequence on each companies in self that will ensure
